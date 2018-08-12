a6a1c80dc9a95c31fa3d26101d8aaf62f790e840
kumar.sendhi <kumar.sendhi@gmail.com>
2018-08-12 21:19:44 +0000

    updated content

--- Kubernetes1/Startup.cs
+++ Kubernetes1/Startup.cs
@@ -27,7 +27,7 @@ namespace Kubernetes1
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World! Kumar");
+                await context.Response.WriteAsync("Hello World! Kumar we are close to winning");
             });
         }
     }
